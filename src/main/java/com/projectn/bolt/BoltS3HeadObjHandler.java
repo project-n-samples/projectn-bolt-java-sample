58e9c174b9fedac3ebe995f42d771f4e16b1951a
Brendan <bc@projectn.co>
2020-10-14 15:20:24 -0400

    feat(SUP-1787): Lambda UAT Integration support part 1
    - Support Json returns.
    - If lambda error's it sends response with error message.
    - Returns MD5 hash of object from get-object call instead of object
    contents.

--- src/main/java/com/projectn/bolt/BoltS3HeadObjHandler.java
+++ src/main/java/com/projectn/bolt/BoltS3HeadObjHandler.java
@@ -4,14 +4,16 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.HeadObjectRequest;
+import software.amazon.awssdk.services.s3.model.HeadObjectResponse;
 import software.amazon.awssdk.services.s3.model.S3Exception;
 
 import java.util.Map;
+import java.util.HashMap;
 
-public class BoltS3HeadObjHandler implements RequestHandler<Map<String,String>, String> {
+public class BoltS3HeadObjHandler implements RequestHandler<Map<String,String>, Map<String,String>> {
 
     @Override
-    public String handleRequest(Map<String,String> event, Context context) {
+    public Map<String,String> handleRequest(Map<String,String> event, Context context) {
 
         String bucket = event.get("bucket");
         String key = event.get("key");
@@ -29,12 +31,24 @@ public class BoltS3HeadObjHandler implements RequestHandler<Map<String,String>, 
         }
 
         HeadObjectRequest headObjectRequest = HeadObjectRequest.builder().bucket(bucket).key(key).build();
-        String response = "";
         try {
-            response = s3.headObject(headObjectRequest).toString();
+            HeadObjectResponse res = s3.headObject(headObjectRequest);
+            Map<String,String> map = new HashMap<String, String>() {{
+                put( "Expiration", res.expiration());
+                put( "lastModified", res.lastModified().toString() );
+                put( "ContentLength", res.contentLength().toString() );
+                put( "ETag", res.eTag() );
+                put( "VersionId", res.versionId() );
+                put( "StorageClass", res.storageClass().toString() );
+            }};
+            return map;
         } catch (S3Exception e) {
-            System.err.println(e.awsErrorDetails().errorMessage());
+            String msg = e.awsErrorDetails().errorMessage();
+            System.err.println(msg);
+            Map<String,String> map = new HashMap<String, String>() {{
+                put("errorMessage", msg);
+            }};
+            return map;
         }
-        return response;
     }
 }
